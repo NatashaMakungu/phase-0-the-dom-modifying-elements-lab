64553ffd0f4afa32ee51bd5c9e5b8e7a1ac440a8
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-03-28 17:08:39 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+let main = document.querySelector('#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Natasha Makungu is the champion'
+
+
+
+
+
+
+
+  
